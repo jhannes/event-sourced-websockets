abf8989fa595658b5a91cbe5565f3953dc820baf
Johannes Brodwall <johannes@brodwall.com>
2023-05-26 01:12:58 +0200

    Load content from src if available

--- src/main/java/com/soprasteria/eventsource/PatchedDefaultServlet.java
+++ src/main/java/com/soprasteria/eventsource/PatchedDefaultServlet.java
@@ -2,16 +2,24 @@ package com.soprasteria.eventsource;
 
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.util.resource.ResourceFactory;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class PatchedDefaultServlet extends DefaultServlet {
 
     private final ResourceFactory resourceFactory;
 
-    public PatchedDefaultServlet(String resource) {
-        this.resourceFactory = Resource.newClassPathResource(resource);
+    public PatchedDefaultServlet(String path) {
+        var sourceResource = Resource.newResource(Path.of("src", "main", "resources", path));
+
+        if (sourceResource.exists()) {
+            this.resourceFactory = new ResourceCollection(sourceResource, Resource.newClassPathResource(path));
+        } else {
+            this.resourceFactory = Resource.newClassPathResource(path);
+        }
     }
 
     @Override
