8629b5df76dfb02033e1501c28fece4667d3fc4c
Johannes Brodwall <johannes@brodwall.com>
2023-05-26 22:21:42 +0200

    Serve code with parcel and React

---
+++ src/main/frontend/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const reactDom = ReactDOM.createRoot(document.getElementById("root")!);
+reactDom.render(<h1>Hello Parcel application</h1>);
