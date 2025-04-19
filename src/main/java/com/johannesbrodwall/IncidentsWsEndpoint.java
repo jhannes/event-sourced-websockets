ceeaefee681c48bcb7a07d92bf63c76d433dfa96
Johannes Brodwall <johannes@brodwall.com>
2025-04-19 17:14:56 +0200

    publish incident messages to client

--- src/main/java/com/johannesbrodwall/IncidentsWsEndpoint.java
+++ src/main/java/com/johannesbrodwall/IncidentsWsEndpoint.java
@@ -1,12 +1,20 @@
 package com.johannesbrodwall;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.websocket.Endpoint;
 import jakarta.websocket.EndpointConfig;
 import jakarta.websocket.Session;
+import lombok.SneakyThrows;
+import org.openapitools.client.model.SampleModelData;
 
 public class IncidentsWsEndpoint extends Endpoint {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final SampleModelData sampleData = new SampleModelData(-1);
+
+    @SneakyThrows
     @Override
     public void onOpen(Session session, EndpointConfig config) {
-        session.getAsyncRemote().sendText("Hello from server");
+        session.getAsyncRemote().sendText(mapper.writeValueAsString(sampleData.sampleIncidentSummaryListDto()));
     }
 }
--- src/test/java/com/johannesbrodwall/IncidentsWsEndpointTest.java
+++ src/test/java/com/johannesbrodwall/IncidentsWsEndpointTest.java
@@ -1,5 +1,6 @@
 package com.johannesbrodwall;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.websocket.ClientEndpointConfig;
 import jakarta.websocket.ContainerProvider;
 import jakarta.websocket.Endpoint;
@@ -7,13 +8,15 @@ import jakarta.websocket.EndpointConfig;
 import jakarta.websocket.Session;
 import org.eclipse.jetty.util.BlockingArrayQueue;
 import org.junit.jupiter.api.Test;
+import org.openapitools.client.model.IncidentSummaryListDto;
 
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class IncidentsWsEndpointTest {
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     private final EventSourcingServer server = new EventSourcingServer(0);
 
@@ -28,7 +31,8 @@ public class IncidentsWsEndpointTest {
         var wsClient = new WsClient();
         try (var connection = ContainerProvider.getWebSocketContainer().connectToServer(wsClient, ClientEndpointConfig.Builder.create().build(), uri)) {
             var message = wsClient.poll(5, TimeUnit.SECONDS);
-            assertThat(message).isEqualTo("Hello from server");
+            var summaries = mapper.readValue(message, IncidentSummaryListDto.class);
+            assertThat(summaries.getSummaries()).isNotEmpty();
         }
     }
 
