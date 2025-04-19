3650ddc25240f8c9531fdb8b67b7ab51ee1cc82d
Johannes Brodwall <johannes@brodwall.com>
2025-04-19 17:38:18 +0200

    send commands from client

--- src/main/frontend/src/modules/app/application.tsx
+++ src/main/frontend/src/modules/app/application.tsx
@@ -1,32 +1,59 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
   IncidentSummaryDto,
   MessageFromServerDto,
+  MessageToServerDto,
 } from "../../../../../../target/generated-sources/openapi-typescript";
+import { v4 as uuidv4 } from "uuid";
 
-function useWebSocket<OUTPUT>(params: {
+function useWebSocket<OUTPUT, INPUT>(params: {
   url: string;
   onMessage: (message: OUTPUT) => void;
 }) {
+  const websocketRef = useRef<WebSocket | null>(null);
   const connect = useCallback(() => {
     const ws = new WebSocket(params.url);
     ws.onmessage = ({ data }) => {
       params.onMessage(JSON.parse(data) as OUTPUT);
     };
+    ws.onopen = () => {
+      websocketRef.current = ws;
+    };
   }, []);
   useEffect(() => {
     connect();
   }, []);
+  function sendMessage(message: INPUT) {
+    websocketRef.current?.send(JSON.stringify(message));
+  }
+  return { sendMessage };
 }
 
 export function Application() {
   const [incidents, setIncidents] = useState<IncidentSummaryDto[]>([]);
 
+  const [description, setDescription] = useState("");
+
   function handleMessage(message: MessageFromServerDto) {
     setIncidents(message.summaries);
   }
 
-  useWebSocket({ url: "/ws/incidents", onMessage: handleMessage });
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    sendMessage({
+      clientTime: new Date(),
+      incidentId: uuidv4(),
+      delta: { description },
+    });
+  }
+
+  const { sendMessage } = useWebSocket<
+    MessageFromServerDto,
+    MessageToServerDto
+  >({
+    url: "/ws/incidents",
+    onMessage: handleMessage,
+  });
 
   return (
     <>
@@ -36,6 +63,23 @@ export function Application() {
           <li key={m.id}>{m.description}</li>
         ))}
       </ul>
+      <h2>New incident</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Description: </strong>{" "}
+            <input
+              autoFocus
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
     </>
   );
 }
