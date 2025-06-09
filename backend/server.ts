823a8b6225f02e8bac84d3c8c40ad8bc7ac47d9f
Johannes Brodwall <johannes@brodwall.com>
2025-06-09 14:42:40 +0200

    save incidents on server

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { WebSocketServer, WebSocket } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import {
+  IncidentCommand,
   IncidentSnapshot,
   MessageFromServer,
+  MessageToServer,
 } from "../shared/incidents/incident";
 import { v4 as uuidv4 } from "uuid";
+import { combineAll } from "rxjs";
 
 const app = express();
 
@@ -30,16 +33,38 @@ function sendMessage(peer: WebSocket, message: MessageFromServer) {
   peer.send(JSON.stringify(message));
 }
 
+function handleMessageToServer(message: MessageToServer) {
+  const command: IncidentCommand = message;
+  const { incidentId, delta } = message;
+  const updatedAt = new Date();
+  if (delta.type === "CreateIncident") {
+    const { incident } = delta;
+    incidents.push({ incidentId, updatedAt, incident });
+  } else if (delta.type === "UpdateIncident") {
+    for (const o of incidents) {
+      if (o.incidentId === incidentId) {
+        o.updatedAt = updatedAt;
+        o.incident = { ...o.incident, ...delta.incident };
+      }
+    }
+  } else {
+    const unhandled: never = delta;
+    console.warn({ unhandled });
+  }
+
+  const event = { ...command, serverTime: updatedAt };
+  for (const peer of peers) {
+    sendMessage(peer, event);
+  }
+}
+
 server.on("upgrade", (req, socket, head) => {
   wsServer.handleUpgrade(req, socket, head, (socket) => {
     peers.add(socket);
     sendMessage(socket, incidents);
     socket.onclose = () => peers.delete(socket);
     socket.onmessage = (event) => {
-      const message = JSON.parse(event.data.toString());
-      for (const peer of peers) {
-        sendMessage(peer, message);
-      }
+      handleMessageToServer(JSON.parse(event.data.toString()));
     };
   });
 });
