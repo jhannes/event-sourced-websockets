65f3c7a29fcc080ffa83439bc2fb4ec642d4f80d
Johannes Brodwall <johannes@brodwall.com>
2025-08-31 21:10:03 +0200

    handle UpdateIncidentDelta (on server side)

--- src/main/java/as/gnist/javazone/IncidentReactor.java
+++ src/main/java/as/gnist/javazone/IncidentReactor.java
@@ -3,35 +3,32 @@ package as.gnist.javazone;
 import as.gnist.javazone.incident.generated.model.CreateIncidentDto;
 import as.gnist.javazone.incident.generated.model.IncidentCommandDto;
 import as.gnist.javazone.incident.generated.model.IncidentEventDto;
-import as.gnist.javazone.incident.generated.model.IncidentInfoDto;
 import as.gnist.javazone.incident.generated.model.IncidentSnapshotDto;
 import as.gnist.javazone.incident.generated.model.IncidentSummaryListDto;
 import as.gnist.javazone.incident.generated.model.MessageToServerDto;
 import as.gnist.javazone.incident.generated.model.SampleModelData;
+import as.gnist.javazone.incident.generated.model.UpdateIncidentDto;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class IncidentReactor {
 
-    private final SampleModelData sampleData = new SampleModelData(-1);
-    private final ArrayList<IncidentSnapshotDto> incidents = new ArrayList<>(List.of(
-            sampleData.sampleIncidentSnapshotDto().setInfo(sampleData.sampleIncidentInfoDto().setSummary("Fire")))
-    );
+    private final Map<UUID, IncidentSnapshotDto> incidents = new HashMap<>();
     private final HashSet<IncidentListener> listeners = new HashSet<>();
+
+    public IncidentReactor() {
+        var sampleData = new SampleModelData(-1);
+        addIncident(sampleData.sampleIncidentSnapshotDto().setInfo(sampleData.sampleIncidentInfoDto().setSummary("Fire")));
+    }
 
     public void handle(MessageToServerDto message) {
         if (message instanceof IncidentCommandDto command) {
-            if (command.getDelta() instanceof CreateIncidentDto create) {
-                incidents.add(new IncidentSnapshotDto()
-                        .setId(command.getIncidentId())
-                        .setCreatedAt(command.getClientTime())
-                        .setUpdatedAt(command.getClientTime())
-                        .setInfo(create.getInfo())
-                );
-            }
+            handleCommand(command);
             var event = new IncidentEventDto().putAll(command).setServerTime(OffsetDateTime.now());
             for (var listener : listeners) {
                 listener.sendMessage(event);
@@ -39,8 +36,27 @@ public class IncidentReactor {
         }
     }
 
+    private void handleCommand(IncidentCommandDto command) {
+        switch (command.getDelta()) {
+            case CreateIncidentDto create -> addIncident(new IncidentSnapshotDto()
+                    .setId(command.getIncidentId())
+                    .setCreatedAt(command.getClientTime())
+                    .setUpdatedAt(command.getClientTime())
+                    .setInfo(create.getInfo()));
+            case UpdateIncidentDto update -> {
+                incidents.get(command.getIncidentId())
+                        .setUpdatedAt(command.getClientTime())
+                        .getInfo().putAll(update.getInfo());
+            }
+        }
+    }
+
+    private void addIncident(IncidentSnapshotDto incident) {
+        incidents.put(incident.getId(), incident);
+    }
+
     public void subscribe(IncidentListener listener) {
         listeners.add(listener);
-        listener.sendMessage(new IncidentSummaryListDto().setIncidents(incidents));
+        listener.sendMessage(new IncidentSummaryListDto().setIncidents(incidents.values().stream().toList()));
     }
 }
